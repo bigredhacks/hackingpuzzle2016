722bdcceef75b96238b9db39c68b2f89fa99ddf4
Trevor Edwards <tre23@cornell.edu>
2016-07-22 23:48:16 -0400

    update solution for c puzzle

--- puzzle4/solution.c
+++ puzzle4/solution.c
@@ -6,20 +6,21 @@ void _magic(char * w11, char * t202, char * f125, char * is425, char * this212) 
 }
 
 void ___theOneThingYouCannotGetBack() {
-	char * _ = "ggoy"; // c
-	char * __ = "ooo"; // i
-	char * ___ = "ddddd"; // w
-	char * ____ = "luck"; // t
-	char * _____ = ":)"; // s
-	_magic(_,__,___,____,_____);
+	char * _ = "chunk"; // c
+	char * __ = "idea"; // i
+	char * ___ = "worry"; // w
+	char * ____ = "the"; // t
+	char * _____ = "see"; // s
+	_magic(_,__,___,____,_____); // under the see
+
 }
 
 
 void main(char * args) {
 	int me, you, kazoo;
-	me = Radium;
-	you = Cornell - 18;
-	kazoo = 66 116 1032 2220; // 12 8 4 3
-	printf("%c%cwwZ56xz8%c\n",me,kazoo,you);	
+	me = 88;
+	you = 65;
+	kazoo = 78; // 12 8 4 3
+	printf("%c%cwwZ56xz8%c\n",me,kazoo,you); //XNwwZ56xz8A
 	___theOneThingYouCannotGetBack();
 }
